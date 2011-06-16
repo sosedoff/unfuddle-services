d92ae66e73a578146bca3fbf89927ca31392f251
Dan Sosedoff <dan.sosedoff@gmail.com>
2011-06-16 01:35:29 -0500

    push request processing tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -16,4 +16,25 @@ describe 'Application' do
   it 'should raise UnfuddleServices::InvalidConfigError if repo config was not found' do
     proc { UnfuddleServices.load_config(12345) }.should raise_error UnfuddleServices::InvalidConfigError
   end
+  
+  it 'should process empty push request' do
+    post '/push'
+    last_response.status.should == 400
+    last_response.body.should match /changeset xml required/i
+  end
+  
+  it 'should process invalid push request' do
+    post '/push', 'some stupid data'
+    last_response.status.should == 400
+    last_response.body.should match /invalid xml data/i
+    
+    
+    post '/push', fixture('sample.xml')
+    last_response.status.should == 400
+    last_response.body.should match /invalid changeset/i
+    
+    post '/push', fixture('invalid_changeset.xml')
+    last_response.status.should == 400
+    last_response.body.should match /invalid changeset/i
+  end
 end
