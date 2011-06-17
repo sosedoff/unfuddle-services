0e7f0084b619b7a3da2285b28d7fc207b217ce26
Dan Sosedoff <dan.sosedoff@gmail.com>
2011-06-16 19:07:20 -0500

    new service: campfire

---
+++ lib/services/campfire.rb
@@ -0,0 +1,28 @@
+module Services
+  class Campfire < Services::Base
+    def initialize(config={})
+      @config     = config
+      @subdomain  = config[:subdomain]
+      @api_token  = config[:api_token]
+      @room       = config[:room]
+      @url        = "https://#{@api_token}:X@{@subdomain}.campfirenow.com/room/#{@room}/speak.json"
+    end
+    
+    def render(data)
+      msg  = "<b>Commit</b><br/>"
+      msg << "     ID: #{data.commit}<br/>"
+      msg << " Author: #{data.author}<br/>on #{data.date}<br/><br/>"
+      msg << "Message: #{data.message.gsub(/\n/, '<br/>')}"
+      msg
+    end
+    
+    def push(changeset)
+      data = {:message => {:body => render(changeset), :type => "TextMessage"}}.to_json,
+      http_post(@url, data)
+    end
+    
+    def valid?
+      ([:subdomain, :api_token, :project, :post] & @config.keys).size == 4
+    end
+  end
+end
