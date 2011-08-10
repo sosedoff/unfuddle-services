69c8fd6fa43c0b76144704b13b71258039803278
Dan Sosedoff <dan.sosedoff@gmail.com>
2011-08-10 12:47:20 -0500

    fixed basecamp test

--- spec/basecamp_spec.rb
+++ spec/basecamp_spec.rb
@@ -20,7 +20,7 @@ describe Services::Basecamp do
   
   it 'should perform a http request' do
     stub_request(:post, "https://API_TOKEN:X@foobar.basecamphq.com/projects/1/posts/1/comments").
-       with(:body => "comment[body]=%3Cb%3ECommit%3C%2Fb%3E%3Cbr%2F%3E%20%20%20%20%20ID%3A%209ff4adb49339636e63e0faa25d5259313b0f7b7c%3Cbr%2F%3E%20Author%3A%20Dan%20Sosedoff%3Cbr%2F%3Eon%202011-06-14%2019%3A04%3A40%20UTC%3Cbr%2F%3E%3Cbr%2F%3EMessage%3A%20commit%20message").
+       with(:body => "comment[body]=%3Cbr%2F%3E%3Cb%3E%239ff4adb49339636e63e0faa25d5259313b0f7b7c%3C%2Fb%3E%3Cbr%2F%3Eby%20Dan%20Sosedoff%20on%20Tue%2C%20June%2014%20%40%2007%3A04%20PM%3Cbr%2F%3E%3Ci%3Ecommit%20message%3C%2Fi%3E").
        to_return(:status => 200, :body => "", :headers => {})
     
     s = basecamp(
